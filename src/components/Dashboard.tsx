2a5bd8085f20b7b28582e3fd6af5e796d7448f26
v1shwajeet <vishwajeetgehlot3@gmail.com>
2025-11-27 23:53:24 +0530

    Delete File Option

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -15,6 +15,8 @@ const Dashboard = () => {
 
   const {data: files, isLoading} = trpc.getUserFiles.useQuery()
 
+  const { mutate } = trpc.deleteFile.useMutation()
+
   return (
     <main className='mx-auto max-w-7xl md:p-10'>
       <div className='mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0'>
@@ -67,16 +69,16 @@ const Dashboard = () => {
               </div>
 
               <Button
-                // onClick={() =>
-                //   deleteFile({ id: file.id })
-                // }
+                onClick={() =>
+                  deleteFile({ id: file.id })
+                }
                 size='sm'
                 className='w-full'
                 variant='destructive'>
                 <Trash className='h-4 w-4' />
-                {/* {currentlyDeletingFile === file.id ? (<Loader2 className='h-4 w-4 animate-spin' />
+                {currentlyDeletingFile === file.id ? (<Loader2 className='h-4 w-4 animate-spin' />
                 ) : (
-                )} */}
+                )}
               </Button>
             </div>
           </li>
