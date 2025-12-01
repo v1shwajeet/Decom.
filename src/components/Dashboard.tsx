84c84a5f832535affdccc97feba64ec2ed1cc928
v1shwajeet <vishwajeetgehlot3@gmail.com>
2025-12-01 16:25:30 +0530

    prisma mock data view

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -42,7 +42,7 @@ const Dashboard = () => {
             <Link
               href={`/dashboard/${file.id}`}
               className='flex flex-col gap-2'>
-              <div className='pt-6 px-6 flex w-full items-center justify-between space-x-6'>
+              <div className='pt-6 pb-4 px-6 flex w-full items-center justify-between space-x-6'>
                 <div className='h-10 w-10 shrink-0 rounded-full bg-linear-to-r from-cyan-500 to-blue-500' />
                 <div className='flex-1 truncate'>
                   <div className='flex items-center space-x-3'>
